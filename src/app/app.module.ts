eed7a4aaf52dada21d425d909c3a9539c5379733
Michele Rizzello <mrizzello@gmail.com>
2023-08-01 10:20:36 +0200

    Added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { MaterialModule } from './material.module';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { SafeHtmlPipe } from './pipes/sanitize.pipe';
 
+import { AppComponent } from './app.component';
 import { IntroductionComponent } from './components/introduction/introduction.component';
 import { StopwatchComponent } from './components/stopwatch/stopwatch.component';
 import { ScoreStarsComponent } from './components/score-stars/score-stars.component';
@@ -45,7 +44,13 @@ import { InfosComponent } from './pages/infos/infos.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    MaterialModule
+    MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
